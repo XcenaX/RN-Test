06101c5e6cf9004247f4df7c373554bc91f3bb24
XcenaX <40425017+XcenaX@users.noreply.github.com>
2019-06-19 21:16:07 +0600

    Update Users.js

--- serverMeteor/imports/api/Users.js
+++ serverMeteor/imports/api/Users.js
@@ -2,17 +2,21 @@ import { Meteor } from 'meteor/meteor';
 import {Mongo} from 'meteor/mongo';
 import { Accounts } from 'meteor/accounts-base'
 
-export const Users = new Mongo.Collection( 'Users' );
+export const Users = Meteor.users;
 
 Meteor.methods({
   AddUser( data ){
     Accounts.createUser(data);
   },
   UpdateTextUser(textInput){
-    Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.text": textInput}});
+    Meteor.users.update({_id: this.userId}, {$set: {"profile.text": textInput}});
   },
 })
 
 Meteor.publish( 'getAllUsers', () => {
   return Meteor.users.find({})
 })
+// Meteor.publish('currentUser',()=>{
+//   console.log("id: "+this.userId);
+//   return Meteor.users.find({}, {fields: {_id: this.userId}});
+// })
