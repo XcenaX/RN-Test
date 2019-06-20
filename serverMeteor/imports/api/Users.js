2ef55b7c7cd0f521ed87da9c3e1d4e579b260644
XcenaX <40425017+XcenaX@users.noreply.github.com>
2019-06-20 13:29:23 +0600

    Update Users.js

--- serverMeteor/imports/api/Users.js
+++ serverMeteor/imports/api/Users.js
@@ -12,7 +12,6 @@ Meteor.methods({
     Meteor.users.update({_id: this.userId}, {$set: {"profile.text": textInput}});
   },
 })
-Meteor.publish('currentUser',()=>{
-  console.log("id: "+this.userId);
-  return this.userId == undefined ? null : Meteor.users.find({_id:this.userId}, {limit:1, fields: {profile:1}});
+Meteor.publish('currentUser',()=>{  
+  return Meteor.users.find({_id:this.userId}, {limit:1, fields: {profile:1}});
 })
